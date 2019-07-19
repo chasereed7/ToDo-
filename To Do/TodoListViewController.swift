63b872aee35927bce439248c50160dfb1e7d22fc
Carter <carter.reed7@iCloud.com>
2019-07-19 12:54:33 -0500

    add new item functionality complete

--- To Do/TodoListViewController.swift
+++ To Do/TodoListViewController.swift
@@ -8,53 +8,90 @@
 
 import UIKit
 
+//MARK Start of code ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike","Buy Eggos","Destroy Demogorgon"]
+    var itemArray = ["Find Mike","Buy Eggos","Destroy Demogorgon"]
     
+    //MARK - ViewDidLoad method
     
     override func viewDidLoad() {
         
-        super.viewDidLoad()
-
+            super.viewDidLoad()
+ 
     }
 
 
-    //MARK - TableView  Datasource Methods
+    //MARK - NumberOfRowsInSection Section –––––––––––––––––––––––––––––––––––––––––––––––––––––
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+            return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
     
-    cell.textLabel?.text = itemArray[indexPath.row]
-        
-        return cell
+            cell.textLabel?.text = itemArray[indexPath.row]
+
+            return cell
     }
     
-    //MARK - TableView Delegate Methods
+//MARK - DidSelectRowAt IndexPath METHOD –––––––––––––––––––––––––––––––––––––––––––––––––––––––
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
 //        print(itemArray[indexPath.row])
+
         
-         tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         
         if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             
-             tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
             
         } else {
             
-             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
             
         }
         
-        tableView.deselectRow(at: indexPath, animated: true)
+            tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+//MARK - ADD BUTTON PRESSED METHOD ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+    
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+    
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new To Do item", message: "this will create a new item", preferredStyle: .alert)
+    
+        let action = UIAlertAction(title: "add item", style: .default) { (action) in
+            // what will happen once the user cilcks the add item button on our UIAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            self.tableView.reloadData()
+            
+            print("––––––success!––––––√")
+            
+        }
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Create new Item"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+
+            self.present(alert, animated: true, completion: nil)
+        
+    }
+    
     
 }
 
+
+//MARK - End of code for now ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
